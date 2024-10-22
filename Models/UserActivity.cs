8dedaea7ae72f2e30b05485710ca82152036e5c2
Dewan Tajmul Islam Nahid <dewan.tajmul007@outlook.com>
2024-10-22 13:26:24 +0600

    test

---
+++ Models/UserActivity.cs
@@ -0,0 +1,10 @@
+﻿namespace HumanResourceManagement.Models
+{
+    public class UserActivity
+    {
+        public String? CreatedById {  get; set; }
+        public DateTime CreatedOn { get; set; }
+        public String? ModifiedById { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+}
